179b5ab300d449e9008de9a11b92cea7e0f29bab
wolfsoul01 <alejandrof011021@gmail.com>
2024-09-18 10:39:41 -0400

    update analytic

--- src/modules/analytic/analytic.controller.ts
+++ src/modules/analytic/analytic.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AnalyticService } from './analytic.service';
 
 @Controller('analytic')
 export class AnalyticController {
   constructor(private readonly analyticService: AnalyticService) {}
+
+  @Get('available')
+  async getAnalytic() {
+    return this.analyticService.getResumeAvailability();
+  }
+  @Get('licencies')
+  async getLicencies() {
+    return this.analyticService.getResumeLicencies();
+  }
 }
--- src/modules/analytic/analytic.service.ts
+++ src/modules/analytic/analytic.service.ts
@@ -1,4 +1,93 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AnalyticService {}
+export class AnalyticService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getResumeAvailability() {
+    const drivers = await this.prisma.driver.findMany();
+
+    const totalCars = await this.prisma.car.count();
+    const carsAvailable = await this.prisma.car.findMany({
+      where: {
+        available: true,
+      },
+    });
+    const carsNotAvailable = await this.prisma.car.findMany({
+      where: {
+        available: false,
+      },
+    });
+
+    const totalRooms = await this.prisma.room.count();
+
+    const roomsAvailable = await this.prisma.room.findMany({
+      where: {
+        available: true,
+      },
+    });
+
+    const roomsNotAvailable = await this.prisma.room.findMany({
+      where: {
+        available: false,
+      },
+    });
+
+    return {
+      driversAvailable: drivers,
+      driversCount: drivers.length,
+
+      totalCars,
+      carsCount: carsAvailable.length,
+      carsAvailable,
+      carsNotAvailable,
+
+      totalRooms,
+      roomsAvailable,
+      roomsNotAvailable,
+    };
+  }
+
+  async getResumeLicencies() {
+    const A = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'A',
+      },
+    });
+    const B = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'B',
+      },
+    });
+    const C = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'C',
+      },
+    });
+    const C1 = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'C1',
+      },
+    });
+    const D = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'D',
+      },
+    });
+    const D1 = await this.prisma.driver.count({
+      where: {
+        typeLicense: 'D1',
+      },
+    });
+
+    return {
+      A,
+      B,
+      C,
+      C1,
+      D,
+      D1,
+    };
+  }
+}
